1bbe369bc21f634412ade000badb49f81c926c6b
kethavath-sanju <157869930+kethavath-sanju@users.noreply.github.com>
2025-05-09 18:52:11 +0530

    update dut_test.py

--- tests/dut_test.py
+++ tests/dut_test.py
@@ -1,9 +1,53 @@
 import cocotb
-from cocotb.triggers import RisingEdge, Timer
-from cocotb.clock import Clock
+from cocotb.triggers import Timer, RisingEdge, FallingEdge, ReadOnly, NextTimeStep
+from cocotb_bus.drivers import BusDriver
+from cocotb.result import TestFailure
+from cocotb_coverage.coverage import CoverCross, CoverPoint, coverage_db
+from cocotb_bus.monitors import BusMonitor
+import os
+import random
 
+
+
+
+def sb_fn(actual_value):
+    global expected_value, test_failures
+    if not expected_value:
+        print("Warning: Unexpected output received")
+        return
+    expected = expected_value.pop(0)
+    print(f"Expected: {expected}, Actual: {actual_value}")
+    if actual_value != expected:
+        test_failures += 1
+        print("  -> Mismatch detected!")
+
+@CoverPoint("top.a", xf=lambda x, y: x, bins=[0, 1])
+@CoverPoint("top.b", xf=lambda x, y: y, bins=[0, 1])
+@CoverCross("top.cross.ab", items=["top.a", "top.b"])
+def ab_cover(a, b):
+    pass
+
+@CoverPoint("top.inputport.current_w", xf=lambda x: x.get('current_w'), bins=["Idle_w", "Txn_w"])  # only two states as write_rdy is always 1
+@CoverPoint("top.inputport.previous_w", xf=lambda x: x.get('previous_w'), bins=["Idle_w", "Txn_w"])
+@CoverCross("top.cross.input", items=["top.inputport.previous_w", "top.inputport.current_w"])
+def inputport_cover(Txn_w_dict):
+    pass
+
+@CoverPoint("top.outputport.current_r", xf=lambda x: x.get('current_r'), bins=["Idle_r", "Txn_r"])  # only two states as read_rdy is always 1
+@CoverPoint("top.outputport.previous_r", xf=lambda x: x.get('previous_r'), bins=["Idle_r", "Txn_r"])
+@CoverCross("top.cross.output", items=["top.outputport.previous_r", "top.outputport.current_r"])
+def outputport_cover(Txn_r_dict):
+    pass
+
+@CoverPoint("top.read_address", xf=lambda x: x, bins=[0, 1, 2, 3])
+def read_address_cover(address):
+    pass
 
 @cocotb.test()
+
+
+
+
 async def test_dut_fifo_behavior(dut):
     # Start clock
     cocotb.start_soon(Clock(dut.CLK, 10, units="ns").start())
